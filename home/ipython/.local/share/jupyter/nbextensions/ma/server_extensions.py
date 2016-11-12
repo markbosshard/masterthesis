5e998e733fa77fd5f4ff8310235581e4b3cac56f
Michael Schneider <michael.schneider@bbbaden.ch>
2016-11-12 20:59:07 +0100

    Merge ready marking

--- home/ipython/.local/share/jupyter/nbextensions/ma/server_extensions.py
+++ home/ipython/.local/share/jupyter/nbextensions/ma/server_extensions.py
@@ -18,22 +18,25 @@ from server.services.ipy_service_distproject import DistProjectHandler
 from server.services.ipy_service_steps import StepHandler
 from server.services.ipy_service_actions import ActionHandler
 from server.services.ipy_service_events import EventHandler
+from server.services.ipy_service_merge import MergeHandler
 from server.services.ipy_tmpfile import TmpFileHandler
+
 
 class NBExtensionHandler(IPythonHandler):
     """Render the notebook extension configuration interface."""
+
     @web.authenticated
     def get(self):
         ipythondir = get_ipython_dir()
-        nbextensions = os.path.join(ipythondir,'nbextensions')
-        exclude = [ 'mathjax' ]
+        nbextensions = os.path.join(ipythondir, 'nbextensions')
+        exclude = ['mathjax']
         yaml_list = []
         # Traverse through nbextension subdirectories to find all yaml files
         for root, dirs, files in os.walk(nbextensions):
             dirs[:] = [d for d in dirs if d not in exclude]
             for f in files:
                 if f.endswith('.yaml'):
-                    yaml_list.append([ root, f] )
+                    yaml_list.append([root, f])
 
         # Build a list of extensions from YAML file description
         # containing at least the following entries:
@@ -44,78 +47,88 @@ class NBExtensionHandler(IPythonHandler):
         #
         extension_list = []
         for y in yaml_list:
-            stream = open(os.path.join(y[0],y[1]), 'r')
+            stream = open(os.path.join(y[0], y[1]), 'r')
             extension = yaml.load(stream)
 
-            if all (k in extension for k in ('Type', 'Compatibility', 'Name', 'Main', 'Description')):
+            if all(k in extension for k in ('Type', 'Compatibility', 'Name', 'Main', 'Description')):
                 if not extension['Type'].startswith('IPython Notebook Extension'):
                     continue
                 if extension['Compatibility'][0] is not '3':
                     continue
                 # generate URL to extension
-                idx=y[0].find('nbextensions')
+                idx = y[0].find('nbextensions')
                 url = y[0][idx::].replace('\\', '/')
                 extension['url'] = url
                 extension_list.append(extension)
             stream.close()
         json_list = json.dumps(extension_list)
         self.write(self.render_template('nbextensions.html',
-            base_url = self.base_url,
-            extension_list = json_list,
-            page_title="Notebook Extension Configuration"
-            )
-        )
+                                        base_url=self.base_url,
+                                        extension_list=json_list,
+                                        page_title="Notebook Extension Configuration"
+                                        )
+                   )
+
 
 def load_jupyter_server_extension(nbapp):
-    #options.parse_command_line()
-    
+    # options.parse_command_line()
+
     webapp = nbapp.web_app
     base_url = webapp.settings['base_url']
     webapp.add_handlers(".*$", [
         # Page handler for the nbextension page
         (ujoin(base_url, r"/nbextensions/"),
-            NBExtensionHandler),
+         NBExtensionHandler),
         # Websocked used by Side-Comments notebook extension
         (ujoin(base_url, r"/comments"),
-            WebSocketHandler),
+         WebSocketHandler),
         # Page handler for the \new page, responsible for
         # rendering the wizard.
         (ujoin(base_url, r"/new"),
-            New_PageHandler),
+         New_PageHandler),
         # Page handler for the \master page, responsible for
         # rendering the project management page
         (ujoin(base_url, r"/master"),
-            Master_PageHandler),
+         Master_PageHandler),
 
         # Handler for the /distprojects REST service
         (ujoin(base_url, r"/distprojects/(convert)$"),
-            DistProjectHandler),
+         DistProjectHandler),
         (ujoin(base_url, r'/distprojects/(?P<prid>[\-0-9A-Za-z]+$)'),
-            DistProjectHandler),
+         DistProjectHandler),
         (ujoin(base_url, r'/distprojects'),
-            DistProjectHandler),
+         DistProjectHandler),
 
         # Handler for the /actions REST service
         (ujoin(base_url, r'/actions$'),
-            ActionHandler),
+         ActionHandler),
         (ujoin(base_url, r'/actions/(?P<category>category)/(?P<cid>[0-9A-Za-z-]+$)'),
-            ActionHandler),
+         ActionHandler),
         (ujoin(base_url, r'/actions/(?P<tree>tree)$'),
-            ActionHandler),
+         ActionHandler),
         (ujoin(base_url, r'/actions/(?P<acid>[\-0-9A-Za-z]+$)'),
-            ActionHandler),
+         ActionHandler),
 
         # Handler for the /events REST service
         (ujoin(base_url, r'/events'),
-            EventHandler),
+         EventHandler),
         (ujoin(base_url, r'/events/(?P<recent>recent/)?(?P<gid>[\-0-9A-Za-z]+$)'),
-            EventHandler),
+         EventHandler),
+
+        # Handler for the /merge REST service
+        #(ujoin(base_url, r'/merge/(?P<prid>[\-0-9A-Za-z]+$/)/(?P<bundle>[0-9]+$)'),
+        # MergeHandler),
+        # Handler for the /merge REST service
+        (ujoin(base_url, r'/merge$'),
+         MergeHandler),
+        (ujoin(base_url, r'/merge/(?P<pid>[\-0-9A-Za-z]+)/(?P<bid>[\-0-9A-Za-z]+$)'),
+         MergeHandler),
 
         (ujoin(base_url, r'/tmpfile'),
-            TmpFileHandler),
+         TmpFileHandler),
         # Handler for the /steps REST service
         (ujoin(base_url, r'/steps'),
-            StepHandler),
+         StepHandler),
         (ujoin(base_url, r'/steps/(?P<sid>[\-0-9A-Za-z]+$)'),
-            StepHandler)
+         StepHandler)
     ])
